65e4f44bf05679b25c2a883bf3ad0ff8b5fdeb04
Alex Blake <alexblake544@gmail.com>
2026-01-20 21:45:16 -0800

    Add advanced mathematical operations

--- calculator.py
+++ calculator.py
@@ -1,11 +1,45 @@
 def add(num1, num2):
-    return num1 + num2
+    try: 
+        return float(num1) + float(num2)
+    except ValueError: 
+        print("Invalid input.")
+
 
 def subtract(num1, num2):
-    return num1 - num2
+    try: 
+        return float(num1) - float(num2)
+    except ValueError: 
+        print("Invalid input.")
+
 
 def multiply(num1, num2):
-    return num1 * num2
+    try: 
+        return float(num1) * float(num2)
+    except ValueError: 
+        print("Invalid input.")
+
 
 def divide(num1, num2):
-    return num1 / num2
+    try: 
+        return float(num1) / float(num2)
+    except ValueError: 
+        print("Invalid input.")
+    except ZeroDivisionError:
+        print("Can't divide by zero.")
+
+
+def power(num, pow):
+    try: 
+        return float(num) ** float(pow)
+    except ValueError: 
+        print("Invalid input.")
+
+
+def square_root(num):
+    try:
+        num = float(num)
+        if num < 0:
+            raise ValueError("Negative number")
+        return num ** 0.5
+    except ValueError: 
+        print("Invalid input.")
